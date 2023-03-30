dd7a49d4dc7760696605fac2024d5de618693500
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-03-29 23:31:02 -0700

    flake setup

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  # Flake outputs
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    # Overlays enable you to customize the Nixpkgs attribute set
+    overlays = [
+      # Makes a `rust-bin` attribute available in Nixpkgs
+      (import rust-overlay)
+      # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+      # create a Rust environment
+      (self: super: {
+        rustToolchain = super.rust-bin.stable.latest.default;
+      })
+    ];
+
+    # Helper to provide system-specific attributes
+    allSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    # Development environment output
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages =
+          (with pkgs; [
+            # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
+            # rustdoc, rustfmt, and other tools.
+            rustToolchain
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [libiconv]);
+      };
+    });
+  };
+}
